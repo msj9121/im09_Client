0a484c40fc1fa5a344529ca199b82add04fffbed
Sung Jin, Moon <msj9121@gmail.com>
2019-01-30 12:40:04 +0900

    client-master

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        클라이언트
+      </div>
+    );
+  }
+}
+
+export default App;
